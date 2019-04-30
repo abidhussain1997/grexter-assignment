5171a068673209682eff73d82fc98a5b5acbf839
Abid Hussain <abidhussain3008@gmail.com>
2019-04-30 15:32:15 +0530

    Add Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css'
+
+
+export default class Header extends React.Component {
+  render(){
+    return (
+      <header className="header-container">
+        <div className="title">Scrum Board</div>
+      </header>
+    )
+  }
+}
